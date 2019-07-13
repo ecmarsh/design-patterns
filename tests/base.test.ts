ebe6f609e3f3b9cd4f7b394cac4752a4d7513ef5
Ethans-Macbook-Pro <ecmarsh@ucdavis.edu>
2019-07-12 17:46:07 -0700

    Some learning tests for object equality

--- tests/base.test.ts
+++ tests/base.test.ts
@@ -1,12 +1,64 @@
 describe('Base tests', () => {
 
-	test('Simple equality', () => {
-		expect(1 + 2).toEqual(3)
-		expect(false).not.toBe(true)
-		expect(null).toBeNull()
+	describe('Simple equality', () => {
+		test('Equals', () => {
+			expect(1 + 2).toEqual(3)
+		})
+		test('Not to be', () => {
+			expect(false).not.toBe(true)
+		})
+		test('Null', () => {
+			expect(null).toBeNull()
+		})
+		test('"Bool"-sy', () => {
+			expect('').toBeFalsy()
+			expect({}).toBeTruthy()
+			expect(undefined).not.toBeNull()
+		})
 	})
 
-	test('Mocks', () => {
+	describe('Object equality', () => {
+		const o1 = {
+			color: 'blue',
+			sizes: [1, 2, 3],
+		}
+
+		test('Identity', () => {
+			expect(o1 === o1).toBe(true)
+			expect(o1).toMatchObject(o1)
+		})
+
+		test('Copies', () => {
+			const o2 = Object.assign({}, o1)
+			expect(o1 == o2).toBe(false)
+			expect(o2).toMatchObject(o1)
+		})
+
+		test('Nested non-primitive', () => {
+			const o3 = {
+				...o1,
+				sizes: [1, 2, 3],
+			}
+			expect(o1.sizes == o3.sizes).toBe(false)
+			expect(o3).toMatchObject(o1)
+		})
+
+		test('Extra properties', () => {
+			const o4 = {
+				...o1,
+				extras: true
+			}
+			expect(o4).toMatchObject(o1)
+		})
+
+		test('Missing properties', () => {
+			const o6 = { ...o1 }
+			delete o6.sizes
+			expect(o6).not.toMatchObject(o1)
+		})
+	})
+
+	describe('Mocks', () => {
 		const Person: any = {
 			init(name: string) {
 				this.name = name
@@ -20,11 +72,21 @@ describe('Base tests', () => {
 
 		const john = Object.create(Person)
 		john.init('John')
-		john.fetch().then((name: string) =>
-			expect(name).toMatch(/John/))
 
-		john.fetch = jest.fn().mockResolvedValue('Bob')
-		john.fetch().then((name: string) =>
-			expect(name).toMatch(/Bob/))
+		test('Lexical 1', () => {
+			john.fetch().then((name: string) =>
+				expect(name).toMatch(/John/))
+		})
+
+		test('Applied mock', () => {
+			john.fetch = jest.fn().mockResolvedValue('Bob')
+			john.fetch().then((name: string) =>
+				expect(name).toMatch(/Bob/))
+		})
+
+		test('Lexical 2', () => {
+			john.fetch().then((name: string) =>
+				expect(name).not.toMatch(/John/))
+		})
 	})
 })
