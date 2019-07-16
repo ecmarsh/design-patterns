e854eea5993b6f221da0b1fc53bfa2de0b49d420
Ethans-Macbook-Pro <ecmarsh@ucdavis.edu>
2019-07-15 23:00:35 -0700

    Reorganize

--- tests/FactoryMethod.test.ts
+++
@@ -1,8 +0,0 @@
-import { Word, Pages } from '../src/creational'
-
-describe('Factory Method', () => {
-  test('Defers to subclasses', () => {
-    expect(new Word().newDoc()).toMatch(/docx/)
-    expect(new Pages().newDoc()).toMatch(/pages/)
-  })
-})
