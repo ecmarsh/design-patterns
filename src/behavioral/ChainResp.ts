322e649ffcfa746359b73e6b94ab1b0b03737333
ethan-marsh <emarsh55@gmail.com>
2019-07-28 17:20:51 -0700

    Chain of Responsibility

--- src/behavioral/ChainResp.ts
+++ src/behavioral/ChainResp.ts
@@ -1,22 +1,200 @@
+import { isFile } from "@babel/types";
+
 /**
  * Chain of Responsibility
  * 
  * -----------------
  * Usage:
- * 
- * 
- * 
+ * To decouple requester from receiver.
+ * When more than one object may handle a request, and the handler isn't exactl known.
+ * To issue a request to one of several objects.
+ * When set of objects that can handle requests should be specified dynamically.
  * 
  * -----------------
  * Consequences:
- * 
- * 
+ * + Reduced coupling from having to know the exact handler, thereby simplifying object interconnections and reducing necessary references.
+ * + More flexibility in assigning responsibilies to objects. You can easily adjust the chain by adding/removing handlers.
+ * - Receipt isn't guaranteed. Without an explicit receiver, there's no way to know for sure that it will find a handler in the chain.
  * 
  * -----------------
  * Implementation Notes:
- * 
- * 
+ * Two possible ways to implement _successor chain_
+ *   1. Define new links (usually in Handler, possibly Concrete Handlers)
+ *   2. Use existing links. Parent refs can define successors in composites.
+ * _Connecting Sucessors_: Defining the chain by giving the handler a refernce to 'next' to define the chain.
+ * You can hardcode request operations or use a single handler that takes codes/request objects to identify and call corresponding handler.
+ *
  */
 
 
-export {}
+/**
+ * **Handler**
+ * Defines interface for handling requests.
+ * Optionally implements the successor link.
+ */
+interface Handler {
+  handleRequest(file: string): File | Error
+}
+
+/**
+ * **Concrete Handlers**
+ * Handles requests it is responsible for.
+ * Can access its successor.
+ * Handles request if able or forwards request.
+ */
+class RequestHandler implements Handler {
+  public static throwNotFound(target: string): Error {
+    throw ReferenceError(`Cannot find module ${target}.`)
+  }
+
+  public constructor(private dir: Directory) {
+    this.dir = dir
+  }
+
+  /**
+   * _Handle Request Flow_
+   * 1. File requests target: File.request('target')
+   * 2. cwd is file parent or error
+   * 3. Check cwd for node_modules/target.js. Return if found
+   * 4. Check cwd for node_modules/target module. If found:
+   *    a. Resolve w/ package module for `package.json` -> get main
+   *        i. If `package` && `main`, check for `main` file. Return if found.
+   *    b. Check for index.js. Resolve w index if exists.
+   * 5. Try successor
+   *    a. If cwd is root, throw 'NOT_FOUND' 
+   *    a. Set cwd to cwd parent. Return to step 3
+   * 6. Repeat until resolved with file or error
+   */
+  public handleRequest(target: string) {
+    if (!('node_modules' in this.dir.entries)) {
+      return this.tryNext(target)
+    }
+
+    const nodeModules = this.dir.entries['node_modules'] as Directory
+
+    // 1. Check for file named target
+    if (nodeModules.hasFile(target)) {
+      return nodeModules.entries[`${target}.js`] as File
+    }
+
+    // 2. Check for dir named target
+    if (nodeModules.hasDirectory(target)) {
+      const targetModule = nodeModules.entries[target] as Directory
+      const resolvedAsModule = this.resolveAsModule(targetModule)
+      if (resolvedAsModule) {
+        return resolvedAsModule
+      }
+    }
+
+    return this.tryNext(target)
+  }
+
+  protected resolveAsModule(mod: Directory) {
+    // Resolve with package
+    if (mod.hasPackage()) {
+      const pkg = mod.entries['package.json'] as Package
+      if (mod.hasFile(pkg.main.replace('.js', ''))) {
+        return mod.entries[pkg.main] as File
+      }
+    }
+    // Resolve with index
+    if (mod.hasFile('index')) {
+      return mod.entries['index.js'] as File
+    }
+  }
+
+  protected tryNext(target: string): Error | File {
+    if (!this.dir.parent) {
+      return RequestHandler.throwNotFound(target)
+    }
+    this.dir = this.dir.parent
+    return this.handleRequest(target)
+  }
+}
+
+
+/**
+ * _Client_
+ * Initiates the request to a Concrete Handler on the chain.
+ * The request propogates along the chain until
+ * a handler takes responsibility and handles it.
+ */
+
+/** Represents directory entry, either file or directory */
+export interface Dirent {
+  dName: string
+  parent?: Directory
+  entries?: { [dName: string]: Dirent }
+}
+
+/**
+ * Directory that may contain entries and parent.
+ * Request handler wraps directory to resolve requests
+ * using props/methods of the directory.
+ */
+export class Directory implements Dirent {
+  public parent?: Directory
+  public entries: { [dName: string]: Dirent }
+
+  public constructor(public dName: string) {
+    this.dName = dName
+    this.entries = {}
+  }
+
+  public add(...dirents: Dirent[]): this {
+    for (const dirent of dirents) {
+      dirent.parent = this
+      this.entries[dirent.dName] = dirent
+    }
+    return this
+  }
+
+  public rm(dName: string): boolean {
+    return delete this.entries[dName]
+  }
+
+  public hasDirectory(target: string): boolean {
+    return this.has(target)
+  }
+  public hasFile(target: string): boolean {
+    return this.has(`${target}.js`)
+  }
+  public hasPackage(): boolean {
+    return this.has('package.json')
+  }
+
+  protected has(dName: string): boolean {
+    return dName in this.entries
+  }
+}
+
+/** The simplest directory entry. A leaf in structure.  */
+export class File implements Dirent {
+  public dName: string
+  public parent?: Directory
+  private Handler: typeof RequestHandler
+
+  public constructor(dName: string, Handler = RequestHandler) {
+    this.dName = dName
+    this.Handler = Handler
+  }
+
+  public request(target: string) {
+    if (!this.parent) {
+      return this.Handler.throwNotFound(target)
+    }
+    const handler = new this.Handler(this.parent)
+    return handler.handleRequest(target)
+  }
+}
+
+/** Specified file indicating the "main" file for package. */
+export class Package extends File {
+  public main: string
+  public parent?: Directory
+
+  public constructor(main: string = 'index.js') {
+    super('package.json')
+    this.main = main
+  }
+}
--- src/behavioral/__tests__/ChainResp.test.ts
+++ src/behavioral/__tests__/ChainResp.test.ts
@@ -1,7 +1,342 @@
-import {} from '../ChainResp'
+import { Dirent, Directory, File, Package } from '../ChainResp'
 
-describe('Chain of Responsibility', function testChainResp() {
-  test('TODO', () => {
-    expect(true).toBe(true)
+describe('Chain Resp. Helper Objects', function testChainRespUtils() {
+  test('File initialization', () => {
+    const file = new File('filename')
+    expect(file.dName).toMatch('filename')
+    expect(file.parent).toBeFalsy()
+  })
+
+  test('Package file', () => {
+    const pkg = new Package('index.js')
+    expect(pkg).toBeInstanceOf(File)
+    expect(pkg.dName).toMatch('package.json')
+    expect(pkg.main).toMatch('index.js')
+  })
+
+  test('Directory initialization', () => {
+    const dir = new Directory('dirname')
+    expect(dir.dName).toMatch('dirname')
+    expect(dir.entries).toMatchObject({})
+  })
+
+  test('Add file to directory', () => {
+    const dir = new Directory('dirname'),
+      filename = 'filename',
+      file = new File(filename)
+
+    const addFile = () => dir.add(file)
+    // Should return dir for chaining methods
+    expect(addFile()).toBe(dir)
+    // Dir should be parent of file, and file child of dir
+    expect(file.parent).toBe(dir)
+    expect(dir.entries).toHaveProperty(filename)
+  })
+
+  test('Add directory to directory', () => {
+    const parent = new Directory('parent')
+    const child = new Directory('child')
+    parent.add(child)
+    expect(child.parent).toBe(parent)
+    expect(parent.entries).toHaveProperty('child')
+  })
+
+  test('Add multiple in one call', () => {
+    const dir = new Directory('dir')
+    const entries: Dirent[] = ['f1', 'f2', 'f3'].map(fname => new File(fname))
+    dir.add(...entries)
+    expect(Object.entries(dir.entries)).toHaveLength(3)
+  })
+
+  test('Directory with multiple levels', () => {
+    const parent = new Directory('parent'),
+      child = new Directory('child'),
+      file = new File('file')
+
+    parent.add(child.add(file))
+    expect(parent.entries['child']).toBeTruthy()
+    expect(child.entries['file']).toBeTruthy()
+  })
+
+  describe('Directory hasDirent()', () => {
+    let dir: Directory
+    beforeEach(() => {
+      dir = new Directory('dir')
+    })
+
+    test('hasDirectory()', () => {
+      const childDirname = 'childDir'
+      expect(dir.hasDirectory(childDirname)).toBe(false)
+      dir.add(new Directory(childDirname))
+      expect(dir.hasDirectory(childDirname)).toBe(true)
+    })
+    test('hasFile()', () => {
+      const index = 'index'
+      expect(dir.hasFile(index)).toBe(false)
+      dir.add(new File(`${index}.js`))
+      expect(dir.hasFile(index)).toBe(true)
+    })
+    test('hasPackage()', () => {
+      expect(dir.hasPackage()).toBe(false)
+      dir.add(new Package())
+      expect(dir.hasPackage()).toBe(true)
+    })
+  })
+
+  test('Remove dirent from directory', () => {
+    const parent = new Directory('parent')
+    const childDir = new Directory('childDir')
+    const childFile = new File('childFile.js')
+    parent.add(childDir, childFile)
+    const hasDir = () => parent.hasDirectory('childDir')
+    const hasFile = () => parent.hasFile('childFile')
+
+    expect(hasDir()).toBe(true)
+    expect(hasFile()).toBe(true)
+    // Remove file
+    parent.rm(childFile.dName)
+    expect(hasFile()).toBe(false)
+    expect(hasDir()).toBe(true) // Ensure no side effects
+    // Remove directory
+    parent.rm(childDir.dName)
+    expect(hasDir()).toBe(false)
+  })
+})
+
+describe('Chain of Resp. Handling', function testChainHandlers() {
+  test('Request from file without parent throws', () => {
+    const file = new File('filename')
+    const makeRequest = () => file.request('target')
+    expect(makeRequest).toThrow()
+  })
+
+  test('Simple resolve as file', () => {
+    const dir = new Directory('dir'),
+      nodeModules = new Directory('node_modules'),
+      requester = new File('requester.js'),
+      targetFile = new File('target.js')
+
+    dir.add(requester, nodeModules)
+    nodeModules.add(targetFile)
+
+    /*
+    |- /dir
+    |   |
+    |   | requester.js
+    |   |
+    |   |- /node_modules
+    |       |
+    |       | target.js
+    |
+    */
+
+    const requested = requester.request('target')
+    expect(requested).toBe(targetFile)
+  })
+
+  describe('Resolve as module', function testResolveAsMod() {
+    test('Resolve with package.main', () => {
+      const dir = new Directory('dir'),
+        nodeModules = new Directory('node_modules'),
+        targetDir = new Directory('target'),
+        pkg = new Package('main.js'),
+        main = new File('main.js'),
+        requester = new File('requester.js')
+
+      dir.add(requester, nodeModules.add(targetDir.add(pkg, main)))
+
+      /*
+      |- /dir
+      |   |
+      |   | requester.js
+      |   |
+      |   |- /node_modules
+      |        |
+      |        |- /target
+      |            | main.js
+      |            | package.json
+      |
+      */
+
+      const requested = requester.request('target')
+      expect(requested).toBe(main)
+    })
+
+    test('Resolve with index', () => {
+      const dir = new Directory('dir'),
+        nodeModules = new Directory('node_modules'),
+        targetDir = new Directory('target'),
+        index = new File('index.js'),
+        requester = new File('requester.js')
+
+      dir.add(requester, nodeModules.add(targetDir.add(index)))
+
+      /*
+      |- /dir
+      |   |
+      |   | requester.js
+      |   |
+      |   |- /node_modules
+      |        |
+      |        |- /target
+      |        |   |
+      |            | index.js
+      |
+      */
+
+      const requested = requester.request('target')
+      expect(requested).toBe(index)
+    })
+  })
+
+  test('Resolves in next', () => {
+    const root = new Directory('root'),
+      src = new Directory('src'),
+      nodeModules = new Directory('node_modules'),
+      target = new File('target.js'),
+      requester = new File('requester.js')
+
+    root.add(src.add(requester), nodeModules.add(target))
+
+    /*
+    |- /root
+    |   |
+    |   |- /src
+    |   |    |
+    |   |    | requester.js
+    |   |
+    |   |- /node_modules
+    |        |
+    |        | target.js
+    |
+    */
+
+    const requested = requester.request('target')
+    expect(requested).toBe(target)
+  })
+
+  test('Multiple-level traversal', () => {
+    const rootDir = new Directory('root'),
+      nodeModulesRoot = new Directory('node_modules'),
+      targetDir = new Directory('target'),
+      index = new File('index.js'),
+      midDir = new Directory('mid'),
+      nodeModulesMid = new Directory('node_modules'),
+      notTarget = new File('notTarget.js'),
+      lowDir = new Directory('low'),
+      requester = new File('requester.js')
+
+    rootDir.add(
+      midDir.add(
+        nodeModulesMid.add(
+          notTarget
+        ),
+        lowDir.add(
+          requester
+        )
+      ),
+      nodeModulesRoot.add(
+        targetDir.add(
+          index
+        )
+      )
+    )
+
+    /*
+    |- /rootDir
+    |   |
+    |   |- /mid
+    |   |    |
+    |   |    |- /node_modules
+    |   |    |   | 
+    |   |    |   | notTarget.js 
+    |   |    | 
+    |   |    |- /low
+    |   |    |   |
+    |   |    |   | requester.js <--initiates
+    |   |
+    |   |- /node_modules
+    |        |
+    |        |- /target
+    |            |
+    |            | index.js  <--expected
+    |
+    */
+
+    const requested = requester.request('target')
+    expect(requested).toBe(index)
+  })
+
+  describe('Chain order', () => {
+    const root = new Directory('root'),
+      src = new Directory('src'),
+      requester = new File('requester.js'),
+      nodeModules = new Directory('node_modules'),
+      targetFile = new File('target.js'),
+      targetDir = new Directory('target'),
+      pkg = new Package('main.js'),
+      main = new File('main.js'),
+      index = new File('index.js')
+
+    root.add(
+      src.add(requester),
+      nodeModules.add(
+        targetFile,
+        targetDir.add(main, pkg, index)
+      )
+    )
+
+    const makeRequest = () => requester.request('target')
+
+    /* Initial structure (number is delete order)
+    |- /root
+    |   |
+    |   |- /src
+    |   |    |
+    |   |    | requester.js
+    |   |
+    |   |- /node_modules (6) -> throw
+    |        |
+    |        | target.js (1)
+    |        |
+    |        |- /target (5) -> throw
+    |        |    |
+    |        |    | main.js (2)
+    |        |    | package.json (3)
+    |        |    | index.js (4) -> throw
+    |
+    */
+
+    test('0. Resolves with target file', () => {
+      const requested = makeRequest()
+      expect(requested).toBe(targetFile)
+    })
+    test('1. Resolves with pkg main if no file', () => {
+      nodeModules.rm(targetFile.dName)
+      const requested = makeRequest()
+      expect(requested).toBe(main)
+    })
+    test('2. Resolves with index if pkg, but no main', () => {
+      targetDir.rm(main.dName)
+      const requested = makeRequest()
+      expect(requested).toBe(index)
+    })
+    test('3. Resolves with index if no pkg', () => {
+      targetDir.rm(pkg.dName)
+      const requested = makeRequest()
+      expect(requested).toBe(index)
+    })
+    test('4. Throws if target module, but no pkg, main, or idx', () => {
+      targetDir.rm(index.dName)
+      expect(makeRequest).toThrow()
+    })
+    test('5. Throws if node_modules, no target module', () => {
+      nodeModules.rm(targetDir.dName)
+      expect(makeRequest).toThrow()
+    })
+    test('6. Throws if no node_modules in root structure', () => {
+      root.rm(nodeModules.dName)
+      expect(makeRequest).toThrow()
+    })
   })
 })
